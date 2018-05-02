a2c0ad088e61b1568a037c980c0f8806314620e9
Aleksey <aleksey.popryaduhin@yahoo.com>
2018-05-02 12:30:45 +0300

    Удалён не нужный код

--- src/app/core/containers/app.component.spec.ts
+++ src/app/core/containers/app.component.spec.ts
@@ -212,12 +212,6 @@ describe('AppComponent', () => {
     })));
 });
 
-/** Create async observable that emits-once and completes
- *  after a JS engine turn */
-export function asyncData<T>(data: T) {
-  return defer(() => Promise.resolve(data));
-}
-
 function advance(fixture: ComponentFixture<any>): void {
   tick();
   fixture.detectChanges();
